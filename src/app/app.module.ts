c0f561ad9c700dc205337861d5205acc8c283477
Haley  Swain <haleyswain@Haleys-MacBook-Pro.local>
2017-01-26 20:54:10 -0800

    create main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { routing } from './app.routing';
+
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { PlayerProfileComponent } from './player-profile/player-profile.component';
+import { AdminComponent } from './admin/admin.component';
+import { EditPlayerComponent } from './edit-player/edit-player.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    PlayerProfileComponent,
+    AdminComponent,
+    EditPlayerComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routing.ts
@@ -0,0 +1,34 @@
+import { ModuleWithProviders }  from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { AdminComponent } from './admin/admin.component';
+import { EditPlayerComponent } from './edit-player/edit-player.component';
+import { HomeComponent } from './home/home.component';
+import { PlayerProfileComponent } from './player-profile/player-profile.component';
+
+
+const appRoutes: Routes = [
+{
+  path: '',
+  component: HomeComponent
+},
+{
+  path: 'about',
+  component: AboutComponent
+},
+{
+  path: 'admin',
+  component: AdminComponent
+},
+{
+  path: 'edit-player',
+  component: EditPlayerComponent
+},
+{
+  path: 'player-profile',
+  component: PlayerProfileComponent
+}
+];
+
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
